887b795bcc51d67c9ba7c06469563614e14b5e8b
Ann Paiuk <annvseslavskaya@gmail.com>
2022-09-07 22:53:51 +0300

    Initial app logic

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var leftSide = 0;
+var rightSide = 500;
+var topSide = 0;
+var bottomSide = 500;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
 };
 
-// Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    var shift = this.speed * dt;
+    this.x = this.x + shift;
+    if (this.x >= rightSide) {
+        this.x = -100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +25,99 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.width = 70;
+Enemy.prototype.height = 70;
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y, speed) {
+    this.sprite = 'images/char-princess-girl.png';
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
+};
 
+Player.prototype.width = 70;
+Player.prototype.height = 70;
+
+Player.prototype.update = function() {
+    const enemy = allEnemies[0];
+
+    const isOverlap = checkIsOverlap(
+        {
+            x: player.x,
+            y: player.y
+        },
+        {
+            x: player.x + player.width,
+            y: player.y + player.height
+        },
+        {
+            x: enemy.x,
+            y: enemy.y
+        },
+        {
+            x: enemy.x + enemy.width,
+            y: enemy.y + enemy.height
+        }
+    );
+
+    if (isOverlap) {
+        gameOver();
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(action) {
+    switch (action) {
+        case 'left': {
+            if (this.x > leftSide) {
+                this.x -= this.speed;
+            }
+            break;
+        }
+        case 'right': {
+            if (this.x + this.width + this.speed < rightSide) {
+                this.x += this.speed;
+            }
+            break;
+        }
+        case 'down': {
+            if (this.y + this.height + this.speed < bottomSide) {
+                this.y += this.speed;
+            }
+            break;
+        }
+        case 'up': {
+            this.y -= this.speed;
+            if (this.y <= 0) {
+                gameOver();
+            }
+
+            break;
+        }
+    }
+}
+
+Player.prototype.setPosition = function(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [
+    new Enemy(100, 200, 100),
+    // new Enemy(-100, 60, 100),
+    // new Enemy(-200, 140, 200), 
+    // new Enemy(-300, 220, 150)
+];
+var player = new Player(200, 400, 75);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +131,21 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function gameOver() {
+    player.setPosition(200, 400)
+}
+
+function checkIsOverlap(l1, r1,  l2,  r2) {
+    const isRectangleOnLeftSideOfAnother = l1.x > r2.x || l2.x > r1.x;
+    if (isRectangleOnLeftSideOfAnother) {
+        return false;
+    }
+
+    const isOneRectangleAboveAnother = r1.y < l2.y || r2.y < l1.y;
+    if (isOneRectangleAboveAnother) {
+        return false;
+    }
+
+    return true;
+}
